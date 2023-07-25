647c7f4d6180e68d0d710205c826d75910efa825
raphael luiz schettino <raphaelluiz128@hotmail.com>
2023-07-25 14:53:35 -0300

    Ajuste para controller de usuarios efetuar operações em BD

--- Controllers/UsuarioController.cs
+++ Controllers/UsuarioController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using webapi.Models;
+using webapi.Repositories.Interfaces;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -9,36 +10,53 @@ namespace webapi.Controllers
     [ApiController]
     public class UsuarioController : ControllerBase
     {
+        private readonly IUsuarioRep? _usuarioRep;
+        
+        public UsuarioController(IUsuarioRep usuarioRep)
+        {
+            _usuarioRep = usuarioRep;
+        }
+
         // GET: api/<UsuariosController>
         [HttpGet]
-        public ActionResult<List<UsuarioModel>> Get()
+        public async Task<ActionResult<List<UsuarioModel>>> Get()
         {
-            return Ok();
+            List<UsuarioModel> usuarios = await _usuarioRep.Get();
+            return Ok(usuarios);
         }
 
         // GET api/<UsuariosController>/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public async Task<ActionResult<UsuarioModel>> BuscarPorId(int id)
         {
-            return "value";
+            UsuarioModel usuario = await _usuarioRep.Get(id);
+            return Ok(usuario);
         }
 
         // POST api/<UsuariosController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public async Task<ActionResult<UsuarioModel>> Cadastrar([FromBody] UsuarioModel usuarioModel)
         {
+            UsuarioModel usuario = await _usuarioRep.Post(usuarioModel);
+            return Ok(usuario);
         }
+
 
         // PUT api/<UsuariosController>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public async Task<ActionResult<UsuarioModel>> Atualizar([FromBody] UsuarioModel usuarioModel, int id)
         {
+            usuarioModel.Id = id;
+            UsuarioModel usuario = await _usuarioRep.Put(usuarioModel, id);
+            return Ok(usuario);
         }
 
         // DELETE api/<UsuariosController>/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public async Task<ActionResult<UsuarioModel>> Apagar(int id)
         {
+            bool apagado = await _usuarioRep.Delete(id);
+            return Ok(apagado);
         }
     }
 }
